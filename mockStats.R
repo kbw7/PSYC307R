87ef83ad27cc41fb6970aef13b5f64d83ba1802f
kbw7 <kb120@wellesley.edu>
2024-11-04 11:11:06 -0500

    new work!

---
+++ mockStats.R
@@ -0,0 +1,25 @@
+#Mock Statistical Analysis
+#install packages
+#install.packages("kableExtra")
+library(kableExtra)
+
+
+#import our mock data set
+d <- read.csv(file.choose()) #original dataset
+df <- read.csv(file.choose()) #this is the dataframe we will be working with
+
+#change effort and gender variable to categorical
+df$effort <- as.factor(df$effort)
+df$gender <- as.factor(df$gender)
+
+#Descriptive Statistics
+#analyze persistence and adult social model ONLY - t-test one-way ANOVA
+tTest <- t.test(df$time, df$effort)
+anova <- aov(time ~ effort, data = df)
+
+#analyze persistence and emotion regulation ONLY - Pearson correlation
+pearCor <- cor(df$time, df$AER, method = 'pearson')
+
+#analyze persistence with both adult social models and emotion regulation, we would run a multiple regression
+multReg <- lm(time~effort * AER, data = df)
+summary(multReg)
