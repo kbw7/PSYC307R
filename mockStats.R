97a11f53bfa047f18e7084c86be5826ad8cdba17
jd109wellesley <jd109@wellesley.edu>
2024-11-07 10:21:56 -0500

    push again

--- mockStats.R
+++ mockStats.R
@@ -1,25 +1,30 @@
 #Mock Statistical Analysis
-#install packages
-#install.packages("kableExtra")
-library(kableExtra)
-
 
 #import our mock data set
-d <- read.csv(file.choose()) #original dataset
-df <- read.csv(file.choose()) #this is the dataframe we will be working with
+d <- read.csv(file.choose()) #original data set
+df <- read.csv(file.choose()) #this is the data frame we will be working with
 
 #change effort and gender variable to categorical
 df$effort <- as.factor(df$effort)
 df$gender <- as.factor(df$gender)
 
 #Descriptive Statistics
-#add here
+
+#age
+summary(df$age)
+sd(df$age)
+#AER
+summary(df$AER)
+sd(df$AER)
+#time
+summary(df$time)
+sd(df$time)
+#gender
+table(df$gender)
 
 #analyze persistence with both adult social models and emotion regulation, we would run a multiple regression
 multReg <- lm(time~effort * AER + age, data = df)
 summary(multReg)
-
-#maybe add age as a predictor var
 
 #graphing our data (simple version)
 plot(df$AER, df$time, col = df$effort)
