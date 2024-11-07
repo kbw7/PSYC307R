4fb27c3d9a53ee96b9e91d616a09c2d1a06a508f
jd109wellesley <jd109@wellesley.edu>
2024-11-07 10:06:41 -0500

    test2

--- mockStats.R
+++ mockStats.R
@@ -13,16 +13,13 @@ df$effort <- as.factor(df$effort)
 df$gender <- as.factor(df$gender)
 
 #Descriptive Statistics
-#analyze persistence and adult social model ONLY - t-test one-way ANOVA
-tTest <- t.test(time~effort, var.equal = TRUE, data = df)
-anova <- aov(time ~ effort, data = df)
-
-#analyze persistence and emotion regulation ONLY - Pearson correlation
-pearCor <- cor(df$time, df$AER, method = 'pearson')
+#add here
 
 #analyze persistence with both adult social models and emotion regulation, we would run a multiple regression
-multReg <- lm(time~effort * AER, data = df)
+multReg <- lm(time~effort~age * AER, data = df)
 summary(multReg)
+
+#maybe add age as a predictor var
 
 #graphing our data (simple version)
 plot(df$AER, df$time, col = df$effort)
